3fd029935aff1521fd95732f8500a2dbd6d5738e
Regina Jackson <rjackson156@gmail.com>
2022-06-21 19:39:07 -0400

    working with uuid for the keys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
-  const [todos, setTodos] = useState(['Todo 12', 'Todo 2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+
+  useEffect (() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+    setTodos()
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button> Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}> Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
     </>
