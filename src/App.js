9b2ddf75acf7596ce4ddf5b25831d0b7ecd8f48a
Regina Jackson <rjackson156@gmail.com>
2022-06-21 16:53:20 -0400

    display todos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-
+import React, { useState } from 'react';
+import TodoList from './TodoList'
 
 function App() {
-  return null
+  const [todos, setTodos] = useState(['Todo 12', 'Todo 2'])
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button> Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
